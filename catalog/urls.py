88a57839fcb1530e4405fd19508acf355bbf4cf0
rahman <mohammedabdulrahman81@gmail.com>
2019-02-04 11:22:44 -1200

    create book

--- catalog/urls.py
+++ catalog/urls.py
@@ -32,7 +32,7 @@ urlpatterns += [
 
 
 urlpatterns += [  
-    path('book/create/', views.BookCreate.as_view(), name='bokk_create'),
+    path('book/create/', views.BookCreate.as_view(), name='book_create'),
     # path('author/<int:pk>/update/', views.AuthorUpdate.as_view(), name='author_update'),
     #path('author/<int:pk>/delete/', views.AuthorDelete.as_view(), name='author_delete'),
 ]
